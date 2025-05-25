5b6a65dc669f5bd3272e61d2260ef1d6653461e9
Xeyla Vithra Arfhina <xeyla@xeyla.xeyla>
2025-05-25 12:53:49 +0700

    fix 2fa

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,5 +1,14 @@
 <div class="bg-white p-[2rem] gap-10 w-[80%] min-h-[80vh] rounded-xl flex">
-    <div class="w-full flex flex-col justify-center items-center">
-        {{ $slot }}
-    </div>
+    @if (isset($left) && isset($right))
+        <div class="w-1/2 flex flex-col justify-center items-center">
+            {{ $left }}
+        </div>
+        <div class="w-1/2 flex flex-col justify-center items-center">
+            {{ $right }}
+        </div>
+    @else
+        <div class="w-full flex flex-col justify-center items-center">
+            {{ $slot }}
+        </div>
+    @endif
 </div>
