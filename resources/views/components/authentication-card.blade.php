5c0d34e2b3d28ae833acd01614879a5a7851b253
Xeyla Vithra Arfhina <xeyla@xeyla.xeyla>
2025-05-25 12:41:41 +0700

    2fa

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,8 +1,5 @@
 <div class="bg-white p-[2rem] gap-10 w-[80%] min-h-[80vh] rounded-xl flex">
-    <div class="w-1/2 flex flex-col justify-center items-center">
-        {{ $left }}
-    </div>
-    <div class="w-1/2 flex flex-col justify-center items-center">
-        {{ $right }}
+    <div class="w-full flex flex-col justify-center items-center">
+        {{ $slot }}
     </div>
 </div>
