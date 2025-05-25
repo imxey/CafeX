cabb1e768bfbb954136ac0e22516fb864ff875c6
Xeyla Vithra Arfhina <xeyla@xeyla.xeyla>
2025-05-25 22:40:36 +0700

    add config cafes

---
+++ config/cafes.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'data' => [
+        'cafe1' => [
+            'latitude' => 40.712776,
+            'longitude' => -74.005974,
+            'menu' => 1,
+            'price' => 1,
+            'wifi_speed' => 1,
+            'distance' => 1,
+            'mosque' => 1,
+        ],
+    ],
+];
